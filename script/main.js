dee2d1ee435c23babe92a21ff05fd91b5388d969
Anton <shvabra01@yandex.ru>
2023-07-06 14:59:05 +0600

    header added

---
+++ script/main.js
@@ -0,0 +1,12 @@
+const headerData = {
+  avatarPath: '../assets/avatar.jpg',
+  name: 'Anton',
+  lastName: 'Chernykh',
+  position: 'IT Projec Manager',
+  contactInformation: {
+    email: 'a.chernykh@pm.me',
+    linkedIn: 'linkedin.com/in/shtraus12/',
+    telegram: '@shraus12',
+    location: 'Location: Kazakhstan (both relocation and remote work are considered)',
+  },
+}
